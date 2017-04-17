753e25354e8ea092031223d7c9d7a2e82aecccd2
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2017-04-17 10:39:36 -0300

    XSS form control

---
+++ application/controllers/xss.php
@@ -0,0 +1,27 @@
+<?php
+
+class XSS extends CI_Controller {
+
+    public function index() {
+
+        $this->load->helper("form");
+
+        echo form_open("xss/show", array("method" => "POST"));
+        echo form_textarea(array("name" => "code"));
+        echo "<br>";
+        echo form_submit(array("value" => "Please, wait!"));
+        echo form_close();
+    }
+
+    public function show() {
+
+        $code = $this->input->post("code");
+
+        //Just with this line the user can send JS code in the textarea and execute that
+        //NOT SAFE!!!!
+        //echo $code;
+        //Line bellow changed in the config/config.php (old = FALSE)
+        //$config['global_xss_filtering'] = TRUE;
+        echo $this->security->xss_clean($code);
+    }
+}
